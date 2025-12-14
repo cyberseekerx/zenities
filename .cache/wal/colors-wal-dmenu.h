0fe2ba252e50191105c25a377ee610efc90b884d
cyberseekerx <kanekarayush777@proton.me>
2025-12-14 13:08:34 +0530

    deleting

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d3f2ef", "#231b30" },
-	[SchemeSel] = { "#d3f2ef", "#3FADC6" },
-	[SchemeOut] = { "#d3f2ef", "#99FEF6" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#231b30";
-static const char* selfgcolor   = "#d3f2ef";
-static const char* normbgcolor  = "#3698F3";
-static const char* normfgcolor  = "#d3f2ef";
-static const char* urgbgcolor   = "#3FADC6";
-static const char* urgfgcolor   = "#d3f2ef";
